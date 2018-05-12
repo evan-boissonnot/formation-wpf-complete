14e74dae830d0fd59f903defcce522d3e9a0d9c8
evanboissonnot <evan@boissonnot.fr>
2018-05-12 15:58:34 +0200

    ajout converters

---
+++ DecouverteWPF/Converters/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Boss001;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converters
+{
+    public class ViewModel : BaseBindable
+    {
+        private bool _isVisible = false;
+
+        public bool IsVisible { get => this._isVisible; set { this._isVisible = value; this.OnPropertyChanged(); } }
+    }
+}
