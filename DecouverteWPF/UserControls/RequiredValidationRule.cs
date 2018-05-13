928615c09d7e0f473225a96ed4c5339ff605ee3b
evanboissonnot <evan@boissonnot.fr>
2018-05-13 17:23:29 +0200

    Module 009 fini

---
+++ DecouverteWPF/UserControls/RequiredValidationRule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Boss001
+{
+    public class RequiredValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+           ValidationResult result = null;
+
+            string content = value.ToString();
+            bool isValid = !string.IsNullOrEmpty(content);
+            result = new ValidationResult(isValid, isValid ? string.Empty : "Champ requis");
+
+            return result;
+        }
+    }
+}
